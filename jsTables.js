1c397a3d08cac451162834b243f686b5503e792c
Terence Tham <terencetmac@gmail.com>
2017-06-03 15:53:17 -0700

    Add ascending and descending sort toggle when clicking a header

--- jsTables.js
+++ jsTables.js
@@ -36,24 +36,71 @@ class JsTable {
 
       tHead.appendChild(tRow);
       this.$el.appendChild(tHead);
+      this.addEventListeners();
     }
 
     // Populate data
     if (this.data) {
-      let tBody = document.createElement('tbody');
-      this.data.forEach((dataRow) => {
-        let tRow = document.createElement('tr');
-        // for sequence in columns
-        this.columns.forEach((column, i) => {
-          // grab the data[key] and populate the td
-          let td = document.createElement('td');
-          td.textContent = dataRow[column['data']];
-          tRow.appendChild(td);
-        });
-        tBody.appendChild(tRow);
-      });
-      this.$el.appendChild(tBody);
+      this.populateTable(this.data);
     }
+  }
+
+  populateTable(data) {
+    // remove original table body
+    const oldBody = this.$el.getElementsByTagName('tbody')[0];
+    if (oldBody) {
+      oldBody.remove();
+    }
+
+    let tBody = document.createElement('tbody');
+    data.forEach((dataRow) => {
+      let tRow = document.createElement('tr');
+      // for sequence in columns
+      this.columns.forEach((column, i) => {
+        // grab the data[key] and populate the td
+        let td = document.createElement('td');
+        td.textContent = dataRow[column['data']];
+        tRow.appendChild(td);
+      });
+      tBody.appendChild(tRow);
+    });
+    this.$el.appendChild(tBody);
+  }
+
+  addEventListeners() {
+    this.$el.addEventListener('click', (e) => {
+      if (e.target.tagName === 'TH') {
+        // if has aria-sort, flip it
+        let ariaSort = e.target.getAttribute('aria-sort') === 'ascending' ? 'descending' : 'ascending';
+        this.sort(e.target.cellIndex, ariaSort);
+
+        let ths = e.target.parentElement.getElementsByTagName('th');
+        for (let i = 0; i < ths.length; i++) {
+          ths[i].removeAttribute('aria-sort');
+        }
+        e.target.setAttribute('aria-sort', ariaSort);
+      }
+    });
+  }
+
+  sort(index, type) {
+    let compare = type === 'ascending' ? 1 : -1;
+    const key = this.columns[index].data;
+    // for each data
+    this.data.sort((a, b) => {
+      let aKey = typeof a[key] === 'string' ? a[key].toUpperCase() : a[key];
+      let bKey = typeof b[key] === 'string' ? b[key].toUpperCase() : b[key]; 
+
+      // compare value of key to be sorted
+      if (aKey < bKey) {
+        return compare;
+      }
+      if (aKey > bKey) {
+        return -1 * compare;
+      }
+      return 0;      
+    });
+    this.populateTable(this.data);
   }
 }
 
