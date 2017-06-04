21717f398fc8a54d68849ade8abf7e2cfe9adbfa
Terence Tham <terencetmac@gmail.com>
2017-06-03 20:16:56 -0700

    Add extra column to sampleData

--- sampleData.js
+++ sampleData.js
@@ -1,35 +1,12 @@
 var sampleData = [
   {
-    name: 'Howard Hatfield',
-    position: 'Office Manager',
-    office: 'San Francisco',
-    age: 51,
-    start_date: '2008/12/16',
-    salary: '$164,500'
-  },
-  {
-    name: 'Sonya Frost',
-    position: 'Software Engineer',
-    office: 'Edinburgh',
-    age: 23,
-    start_date: '2008/12/13',
-    salary: '$103,600'
-  },
-  {
-    name: 'Timothy Mooney',
-    position: 'Office Manager',
-    office: 'London',
-    age: 37,
-    start_date: '2008/12/11',
-    salary: '$136,200'
-  },
-  {
     name: 'Airi Satou',
     position: 'Accountant',
     office: 'Tokyo',
     age: 33,
     start_date: '2008/11/28',
-    salary: '$162,700'
+    salary: '$162,700',
+    email: 'thisemail@gmail.com'
   },
   {
     name: 'Shad Decker',
@@ -37,7 +14,8 @@ var sampleData = [
     office: 'Edinburgh',
     age: 51,
     start_date: '2008/11/13',
-    salary: '$183,000'
+    salary: '$183,000',
+    email: 'thisemail@gmail.com'
   },
   {
     name: 'Gavin Cortez',
@@ -45,7 +23,8 @@ var sampleData = [
     office: 'San Francisco',
     age: 22,
     start_date: '2008/10/26',
-    salary: '$235,500'
+    salary: '$235,500',
+    email: 'thisemail@gmail.com'
   },
   {
     name: 'Charde Marshall',
@@ -53,7 +32,8 @@ var sampleData = [
     office: 'San Francisco',
     age: 36,
     start_date: '2008/10/16',
-    salary: '$470,600'
+    salary: '$470,600',
+    email: 'thisemail@gmail.com'
   },
   {
     name: 'Jackson Bradshaw',
@@ -61,6 +41,295 @@ var sampleData = [
     office: 'New York',
     age: 65,
     start_date: '2008/09/26',
-    salary: '$645,750'
+    salary: '$645,750',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Howard Hatfield',
+    position: 'Office Manager',
+    office: 'San Francisco',
+    age: 51,
+    start_date: '2008/12/16',
+    salary: '$164,500',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Sonya Frost',
+    position: 'Software Engineer',
+    office: 'Edinburgh',
+    age: 23,
+    start_date: '2008/12/13',
+    salary: '$103,600',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Timothy Mooney',
+    position: 'Office Manager',
+    office: 'London',
+    age: 37,
+    start_date: '2008/12/11',
+    salary: '$136,200',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Airi Satou',
+    position: 'Accountant',
+    office: 'Tokyo',
+    age: 33,
+    start_date: '2008/11/28',
+    salary: '$162,700',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Shad Decker',
+    position: 'Regional Director',
+    office: 'Edinburgh',
+    age: 51,
+    start_date: '2008/11/13',
+    salary: '$183,000',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Gavin Cortez',
+    position: 'Team Leader',
+    office: 'San Francisco',
+    age: 22,
+    start_date: '2008/10/26',
+    salary: '$235,500',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Charde Marshall',
+    position: 'Regional Director',
+    office: 'San Francisco',
+    age: 36,
+    start_date: '2008/10/16',
+    salary: '$470,600',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Jackson Bradshaw',
+    position: 'Director',
+    office: 'New York',
+    age: 65,
+    start_date: '2008/09/26',
+    salary: '$645,750',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Howard Hatfield',
+    position: 'Office Manager',
+    office: 'San Francisco',
+    age: 51,
+    start_date: '2008/12/16',
+    salary: '$164,500',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Sonya Frost',
+    position: 'Software Engineer',
+    office: 'Edinburgh',
+    age: 23,
+    start_date: '2008/12/13',
+    salary: '$103,600',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Timothy Mooney',
+    position: 'Office Manager',
+    office: 'London',
+    age: 37,
+    start_date: '2008/12/11',
+    salary: '$136,200',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Airi Satou',
+    position: 'Accountant',
+    office: 'Tokyo',
+    age: 33,
+    start_date: '2008/11/28',
+    salary: '$162,700',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Shad Decker',
+    position: 'Regional Director',
+    office: 'Edinburgh',
+    age: 51,
+    start_date: '2008/11/13',
+    salary: '$183,000',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Gavin Cortez',
+    position: 'Team Leader',
+    office: 'San Francisco',
+    age: 22,
+    start_date: '2008/10/26',
+    salary: '$235,500',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Charde Marshall',
+    position: 'Regional Director',
+    office: 'San Francisco',
+    age: 36,
+    start_date: '2008/10/16',
+    salary: '$470,600',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Jackson Bradshaw',
+    position: 'Director',
+    office: 'New York',
+    age: 65,
+    start_date: '2008/09/26',
+    salary: '$645,750',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Howard Hatfield',
+    position: 'Office Manager',
+    office: 'San Francisco',
+    age: 51,
+    start_date: '2008/12/16',
+    salary: '$164,500',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Sonya Frost',
+    position: 'Software Engineer',
+    office: 'Edinburgh',
+    age: 23,
+    start_date: '2008/12/13',
+    salary: '$103,600',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Timothy Mooney',
+    position: 'Office Manager',
+    office: 'London',
+    age: 37,
+    start_date: '2008/12/11',
+    salary: '$136,200',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Airi Satou',
+    position: 'Accountant',
+    office: 'Tokyo',
+    age: 33,
+    start_date: '2008/11/28',
+    salary: '$162,700',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Shad Decker',
+    position: 'Regional Director',
+    office: 'Edinburgh',
+    age: 51,
+    start_date: '2008/11/13',
+    salary: '$183,000',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Gavin Cortez',
+    position: 'Team Leader',
+    office: 'San Francisco',
+    age: 22,
+    start_date: '2008/10/26',
+    salary: '$235,500',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Charde Marshall',
+    position: 'Regional Director',
+    office: 'San Francisco',
+    age: 36,
+    start_date: '2008/10/16',
+    salary: '$470,600',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Jackson Bradshaw',
+    position: 'Director',
+    office: 'New York',
+    age: 65,
+    start_date: '2008/09/26',
+    salary: '$645,750',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Howard Hatfield',
+    position: 'Office Manager',
+    office: 'San Francisco',
+    age: 51,
+    start_date: '2008/12/16',
+    salary: '$164,500',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Sonya Frost',
+    position: 'Software Engineer',
+    office: 'Edinburgh',
+    age: 23,
+    start_date: '2008/12/13',
+    salary: '$103,600',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Timothy Mooney',
+    position: 'Office Manager',
+    office: 'London',
+    age: 37,
+    start_date: '2008/12/11',
+    salary: '$136,200',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Airi Satou',
+    position: 'Accountant',
+    office: 'Tokyo',
+    age: 33,
+    start_date: '2008/11/28',
+    salary: '$162,700',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Shad Decker',
+    position: 'Regional Director',
+    office: 'Edinburgh',
+    age: 51,
+    start_date: '2008/11/13',
+    salary: '$183,000',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Gavin Cortez',
+    position: 'Team Leader',
+    office: 'San Francisco',
+    age: 22,
+    start_date: '2008/10/26',
+    salary: '$235,500',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Charde Marshall',
+    position: 'Regional Director',
+    office: 'San Francisco',
+    age: 36,
+    start_date: '2008/10/16',
+    salary: '$470,600',
+    email: 'thisemail@gmail.com'
+  },
+  {
+    name: 'Jackson Bradshaw',
+    position: 'Director',
+    office: 'New York',
+    age: 65,
+    start_date: '2008/09/26',
+    salary: '$645,750',
+    email: 'thisemail@gmail.com'
   }
 ];     
